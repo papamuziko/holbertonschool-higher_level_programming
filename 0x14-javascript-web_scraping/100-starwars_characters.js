094b34e0c2b6be7f4eb64dad5bfba42a09b7dc0b
Guillaume SALVA <guillaume@holbertonschool.com>
2020-09-29 12:29:51 -0700

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
 
-request("https://swapi-api.hbtn.io/api/films/" + process.argv[2] + "/?format=json", function (error, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json', function (error, response, body) {
   if (error) {
     console.log(error);
   }
   const jCharacters = JSON.parse(body).characters;
   for (let index = 0; index < jCharacters.length; index++) {
-    request(jCharacters[index] + "?format=json", function(cerror, cresponse, cbody) {
+    request(jCharacters[index] + '?format=json', function (cerror, cresponse, cbody) {
       if (cerror) {
         console.log(cerror);
       }
       const jCharacter = JSON.parse(cbody);
-      console.log(jCharacter.name)
-    }); 
+      console.log(jCharacter.name);
+    });
   }
 });
